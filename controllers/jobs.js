5daf537fcd8f8466ec3bc68e4d0c6977ed6e1591
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-27 12:40:01 +0200

    add reminder how to build query

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,3 +1,33 @@
+/* With a JSON doc - query using JSON DOCS 
+Person.
+  find({
+    occupation: /host/,
+    'name.last': 'Ghost',
+    age: { $gt: 17, $lt: 66 },
+    likes: { $in: ['vaporizing', 'talking'] }
+  }).
+  limit(10).
+  sort({ occupation: -1 }).
+  select({ name: 1, occupation: 1 }).
+  exec(callback);
+
+  // find() - filters
+  // limit() - limit
+  // sort() - sorting
+  // select() - which fields to return
+
+// Using query builder
+  Person.
+    find({ occupation: /host/ }).
+    where('name.last').equals('Ghost').
+    where('age').gt(17).lt(66).
+    where('likes').in(['vaporizing', 'talking']).
+    limit(10).
+    sort('-occupation').
+    select('name occupation').
+    exec(callback);
+*/
+
 const getAllJobs = async (req, res) => {
   res.send("get all jobs");
 };
