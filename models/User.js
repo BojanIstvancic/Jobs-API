75101b860fa49cb27b438d7dc09a40d0a837a232
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-27 11:04:53 +0200

    expiration date and secret add as a secrets

--- models/User.js
+++ models/User.js
@@ -32,9 +32,13 @@ UserSchema.pre("save", async function () {
 });
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
 };
 // we can extend schema and add another methods in methods property
 
